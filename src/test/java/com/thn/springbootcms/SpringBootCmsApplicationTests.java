b501ef65e2eb4a47e1096ded448b22ca9bc0790f
01thn <tikhon.novitski@protonmail.com>
2022-06-22 18:45:14 +0300

    Initial commit

---
+++ src/test/java/com/thn/springbootcms/SpringBootCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thn.springbootcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
